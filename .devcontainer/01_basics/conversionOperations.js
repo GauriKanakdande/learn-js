eb10795a76484e60f059f62a8cac7c3435d6fd77
GauriKanakdande <87631532+GauriKanakdande@users.noreply.github.com>
2024-04-21 12:59:54 +0000

    js basic

---
+++ .devcontainer/01_basics/conversionOperations.js
@@ -0,0 +1,31 @@
+let numberValue
+
+let convertedNumber = Number(numberValue)
+// console.log(typeof convertedNumber)
+// console.log(convertedNumber)
+
+//33 => number 33
+// "" => number 0
+// "gauri" => number NaN 
+// null => number 0
+// undefined => number  NaN
+
+let booleanValue = null
+
+ let convertedBoolean = Boolean(booleanValue)
+// console.log(typeof convertedBoolean)
+// console.log(convertedBoolean)
+
+// "" => boolean false
+// "gauri" => boolean true 
+// null => boolean false
+// undefined => boolean  false
+
+let stringValue 
+
+let convertedString = String(stringValue)
+console.log(typeof convertedString)
+console.log(convertedString)
+
+// null => string null
+// undefined => string  undefined
