41b2be97ae9d58b7afe01c421e0e65f1da7e7517
GauriKanakdande <87631532+GauriKanakdande@users.noreply.github.com>
2024-04-21 12:58:23 +0000

    js basic

---
+++ .devcontainer/01_basics/dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3)  // here node.js is used not browser  
+
+/**
+ * number 
+ * bigint
+ * string
+ * boolean
+ * null => standalone value
+ * undefined => e.g let name; // it does not have any value
+ * symbol
+ * object
+ */
+
+console.log(typeof "gauri") //string
+console.log(typeof undefined)  // undefined
+console.log(typeof null)  // object
---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,14 @@
+console.log("hi")
+
+const accountID = 123
+let accountHolderName = "gauri"
+var accountEmail = "gauri@gmail.com"
+city = "pune"
+
+console.table([accountID, accountHolderName, accountEmail, city])
+
+// const variables cannot be changed 
+// const and let keywords are used to declare variables
+/*
+var is not used coz it does not follow scope
+*/
